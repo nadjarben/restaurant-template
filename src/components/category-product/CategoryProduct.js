0bd669d52bb70f024da2e109d9d167dd40fec6c0
nadjarben <nadjarbenj@gmail.com>
2020-05-24 22:47:51 +0300

    add process.env

--- src/components/category-product/CategoryProduct.js
+++ src/components/category-product/CategoryProduct.js
@@ -6,13 +6,20 @@ import data from '../../category.json';
 
 const CategoryProduct = ({ setCategory }) => {
 
+    let url;
+  if (process.env.NODE_ENV === "development") {
+    url = "http://localhost:3000";
+  } else {
+    url = "https://amazing-poincare-3f19ef.netlify.app";
+  }
+
     return (
         <div>
             <ContainerCategory>
             <TitleCategory>Categories</TitleCategory>
             {data.map((d, id) => (
               <div key={id} onClick={() => setCategory(d.name)}>
-                  <CategoryCard title={d.name} icon={`http://localhost:3000/${d.icon}`} />
+                  <CategoryCard title={d.name} icon={`${url}/${d.icon}`} />
                   <div style={{height: '8px'}} />
               </div>
             ))}
